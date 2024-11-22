3549bcadd9d662eee9424d0f048685b28b239221
Hari Nikesh R <hari.nikesh.r.cce@gmail.com>
2024-11-22 10:44:51 +0530

    Done setup for docker database

--- init.db/postgres/types.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE valid_colors AS ENUM ('red', 'green', 'blue');
-
-CREATE TABLE colors (
-    color VALID_COLORS
-);
--- init.db/sqlserver/V2__movie.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS movie;
-
-CREATE TABLE movie (
-    code VARCHAR(80) PRIMARY KEY,
-    name VARCHAR(80),
-    UNIQUE(name)
-);
